66d0bc1c04a42d01d4186c2eca671368c99c0961
Aaron McConnell <amcconnell@duosecurity.com>
2024-06-07 11:17:15 -0400

    Add function to validate headers

--- duo_hmac/duo_hmac_validation.py
+++ duo_hmac/duo_hmac_validation.py
@@ -1,1 +1,32 @@
-# TODO
+def validate_headers(headers: dict[str, str]):
+  if headers is None:
+    headers = {}
+
+  problems = []
+
+  headers_seen = set()
+
+  for key, value in headers.items():
+    if key is None:
+      problems.append("'None' is not a valid header name.")
+      continue
+    if value is None:
+      problems.append("'None' is not a valid header value")
+      continue
+    if '\x00' in key:
+      problems.append(f"Null characters are not valid in header name {key}")
+      continue
+    if '\x00' in value:
+      problems.append(f"Null characters are not valid in header value {value}")
+      continue
+    
+    key_lower = key.lower()
+    if key_lower.startswith('x-duo'):
+      if key_lower in headers_seen:
+        problems.append(f"Duplicate x-duo headers are not supported, {key_lower} is duplicated.")
+      else:
+        headers_seen.add(key_lower)
+    
+  if problems:
+    problem_string = '\n'.join(problems)
+    raise ValueError(problem_string)
